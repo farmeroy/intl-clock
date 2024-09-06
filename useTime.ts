e9eb6c0f58c500fd92f0fc825548edc49385edd1
Raffaele Cataldo <kemanicataldo@gmail.com>
2024-09-06 15:04:38 -0700

    remove misplaced file

--- useTime.ts
+++
@@ -1,9 +0,0 @@
-import {useContext} from "react";
-
-export const useTime = () => {
-  const context = useContext(TimeContext);
-  if (context === undefined) {
-    throw new Error('useTime must be used within a TimeProvider');
-  }
-  return context;
-};
